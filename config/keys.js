143a228fad534a8243a4ac6ec61e0cfafdc9086b
Christian Camacho <xentyomail@gmail.com>
2018-07-02 10:48:41 -0700

    Resolve mistake in the mongo URI

--- config/keys.js
+++ config/keys.js
@@ -4,6 +4,6 @@ const user = process.env.MONGO_USER,
   db = process.env.MONGO_DB,
   mongoPort = process.env.MONGO_PORT;
 module.exports = {
-  mongoURI: "mongodb://" + user + ":" + password + "@"+ mongoPort +".mlab.com:" + mongoPort + "/" + db,
+  mongoURI: "mongodb://" + user + ":" + password + "@ds"+ mongoPort +".mlab.com:" + mongoPort + "/" + db,
   secretKey: "Bearer"
 };
