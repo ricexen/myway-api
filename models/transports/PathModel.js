60461d129b6de7a820b5542e600b623f5b5341f1
Christian Camacho <xentyomail@gmail.com>
2018-07-02 09:55:09 -0700

    Resolve mongoose models
    Added price model to separate the price from the path model.
    Moved the price from the transport to the path.

--- models/transports/PathModel.js
+++ models/transports/PathModel.js
@@ -1,51 +1,16 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var KeypointSchema = require('mongoose').model('keypoint').schema;
-//var GeoJSON = require('mongoose-geojson-schema');
-
-//meter este Schema, arreglarlo y moverlo a su propia carpeta.
-// var PathSchema = new Schema({
-//     way: {type: "LineString", required: true},
-//     name: {type: String, required: true},
-//     transport: [Transport], //Hacer referencia al modelo de Transporte.
-
-//   });
 
 // Creates a LineString Schema.
 var PathSchema = new Schema({
-	name: { type: String, required: true },
-	geo: {
-		type: {
-			type: String,
-			default: 'LineString'
-		},
-		coordinates: Array
-	},
-	keypoints: [ KeypointSchema ], //Hacer la referencia a point, de mongodb.
-	color: { type: String, min: 3, max: 6 }
+  name: { type: String, required: true },
+  line: {
+    type: "LineString",
+    coordinates: Array
+  },
+  prices: [{ type: Schema.Types.ObjectId, ref: "Price" }],
+  keypoints: [{ type: Schema.Types.ObjectId, ref: "KeyPoint" }],
+  color: { type: String, min: 3, max: 6, required: false }
 });
 
-module.exports = mongoose.model('path', PathSchema);
-
-// const mapItemSchema = new mongoose.Schema({
-//     name: String,
-//     location: {
-//         // It's important to define type within type field, because
-//         // mongoose use "type" to identify field's object type.
-//         type: {type: String, default: 'Point'},
-//         // Default value is needed. Mongoose pass an empty array to
-//         // array type by default, but it will fail MongoDB's pre-save
-//         // validation.
-//         coordinates: {type: [Number], default: [0, 0]}
-//     }
-// });
-// const MapItem = mongoose.model('LineString', mapItemSchema);
-
-// MapItem.create({
-//     name: 'Toronto',
-//     location: {
-//         type: 'Point',
-//         // Place longitude first, then latitude
-//         coordinate: [-79.3968307, 43.6656976]
-//     }
-// });
+module.exports = mongoose.model("path", PathSchema);
--- models/transports/TransportModel.js
+++ models/transports/TransportModel.js
@@ -1,31 +1,16 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 // Path = require('./PathModel.js'),
 // PathSchema = mongoose.model('Path').schema,
 // Schema = mongoose.Schema;
-var PathSchema = require('mongoose').model('path').schema;
+var PathSchema = require("mongoose").model("path").schema;
 //var TransportFamilySchema = require('mongoose').model('transportFamily').schema;
 
 var TransportSchema = new Schema({
-	name: { type: String, required: true },
-	commonName: { type: String, required: true },
-	currency: { type: String, required: true },
-	prices: {
-		dayRate: {
-			rateNormal: { type: Number, required: true },
-			rateStudent: { type: Number },
-			startTime: { type: Date, required: true },
-			endTime: { type: Date, required: true }
-		},
-		nightRate: {
-			rateNormal: { type: Number, required: true },
-			rateStudent: { type: Number },
-			startTime: { type: Date, required: true },
-			endTime: { type: Date, required: true }
-		}
-	},
-	family: { type: String, required: true },
-	locations: [ PathSchema ]
+  name: { type: String, required: true },
+  commonName: { type: String, required: true },
+  family: { type: String, required: true },
+  paths: [{ type: Schema.Types.ObjectId, ref: "Path" }]
 });
 
-module.exports = mongoose.model('transport', TransportSchema);
+module.exports = mongoose.model("transport", TransportSchema);
