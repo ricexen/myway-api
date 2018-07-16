8d5792ba435a672aa0c993d13223c7725d802b0a
Christian Camacho <xentyomail@gmail.com>
2018-07-16 01:02:52 -0700

    Add GeoPoint Schema to avoid _id in each point

--- models/transports/KeyPointModel.js
+++ models/transports/KeyPointModel.js
@@ -1,13 +1,11 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+var GeoPoint = require("./GeoPointSchema");
 
 var KeyPointSchema = new Schema({
   name: { type: String, required: true },
   isBase: { type: Boolean, required: true, default: false },
-  location: {
-    lat: { type: Number, required: true },
-    lng: { type: Number, required: true }
-  }
+  location: { GeoPoint }
 });
 
-module.exports = mongoose.model("keypoint", KeypointSchema);
+module.exports = mongoose.model("keypoint", KeyPointSchema);
