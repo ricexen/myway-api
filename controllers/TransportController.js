619233da07c749e33433e2e023bebccfce78b858
Yolanda Monserrat <yolanda.garcia16@tectijuana.edu.mx>
2018-07-27 12:17:22 -0600

    Cambios conflictos mapa

--- controllers/TransportController.js
+++ controllers/TransportController.js
@@ -1,15 +1,26 @@
 const Transport = require("../models/transports/TransportModel.js");
+const Validation = require("../helpers/Util.js").Validation
 
 module.exports = {
   list(req, res) {
-    Transport.find()
-      .catch(err => res.status(500).send(err))
-      .then(transports => {
-        if (transports.length === 0)
-          res.status(404).send({ message: "Transports not found" });
-        else res.status(200).send(transports);
-      });
+    var command = Transport.find();
+    if (!Validation.isEmpty(req.query)) {
+      var populate = {};
+      if (Boolean(req.query.paths)) {
+        populate.path = "paths";
+        if (Boolean(req.query.prices)) {
+          populate.populate = { path: "prices" };
+        }
+      }
+      command.populate(populate);
+    }
+    command.catch(err => res.status(500).send(err)).then(transports => {
+      if (transports.length === 0)
+        res.status(404).send({ message: "Transports not found" });
+      else res.status(200).send(transports);
+    });
   },
+
   transport(req, res) {
     console.log(req.params)
     Transport.findById(req.params.id)
