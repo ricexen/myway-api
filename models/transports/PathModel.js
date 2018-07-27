93204ce144e2a6ce4db5be489089fe87b6889cf2
Daniel Medina <daniel.medina@tectijuana.edu.mx>
2018-07-27 11:19:29 -0700

    Added virtual property 'geojson' to PathModel

--- models/transports/PathModel.js
+++ models/transports/PathModel.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var GeoPoint = require("./GeoPointSchema");
+var basePath = require("./basePath");
 
 var PathSchema = new Schema({
   name: { type: String, required: true },
@@ -10,6 +11,16 @@ var PathSchema = new Schema({
   color: { type: String, min: 3, max: 6, required: false },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
+}, { toObject: { virtuals:true }, toJSON: { virtuals: true }});
+PathSchema.virtual('geojson').get(function () {
+  var coords = [];
+  for (var j = 0; j < this.line.length; j++) {
+    coords.push([this.line[j].lon, this.line[j].lat]);
+  }
+  var geojson = JSON.parse(JSON.stringify(basePath));
+  geojson.features[0].properties.name = this.name;
+  geojson.features[0].geometry.coordinates = coords;
+  return geojson;
 });
 
 module.exports = mongoose.model("path", PathSchema);
---
+++ models/transports/basePath.js
@@ -0,0 +1,20 @@
+var baseGeoJSON = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        name: "NOMBRE",
+        desc: "Length: 8.63 km (5.362 mi)",
+        color: "white"
+      },
+      geometry: {
+        type: "LineString",
+        coordinates: [
+          [],
+        ]
+      }
+    }
+  ]
+};
+module.exports = baseGeoJSON
