6e5ecbc34cc4dc4f042cde1e8cce21c3871dfaab
Daniel Medina <daniel.medina@tectijuana.edu.mx>
2018-08-03 12:14:21 -0700

    Added shortestDistance method

--- controllers/PathController.js
+++ controllers/PathController.js
@@ -4,6 +4,13 @@ const Path = require("../models/transports/PathModel.js");
 const KeyPoint = require("../models/transports/KeyPointModel.js");
 const Transport = require("../models/transports/TransportModel.js");
 const Price = require("../models/transports/PriceModel.js");
+const mbxDirections = require('@mapbox/mapbox-sdk/services/directions');
+const directionsClient = mbxDirections({
+  accessToken: 'pk.eyJ1IjoiZGFuaWVsbWVkaW5hIiwiYSI6ImNqajYxMXFyNDBjbWUzcXN3bmk3Z2JqcjAifQ.KfFBvYL667g9_-gblXgvcw'
+});
+const nearestPointOnLine = require('@turf/nearest-point-on-line').default;
+const distance = require('@turf/distance').default;
+const turfHelpers = require('@turf/helpers');
 
 module.exports = {
   paths(req, res) {
@@ -103,5 +110,48 @@ module.exports = {
     KeyPoint.find({ name: req.params.name }).catch(err =>
       res.status(500).send(err)
     );
+  },
+  estimatedTranportArrival(req, res) {
+    Path.findById(req.pathId).exec((err, path) => {
+      if(err){ 
+        res.status(404).send("Path not found");
+      }
+      else{
+        
+      }
+    });
+    directionsClient.getDirections({
+      waypoints: [
+        {
+          coordinates: [-117.12008, 32.527522]
+        },
+        {
+          coordinates: [-117.040174, 32.534628]
+        }
+      ],
+      geometries: 'geojson',
+      profile: 'driving',
+      overview: 'full'
+    })
+    .send()
+    .then(response => {
+      const route = response.body;
+      res.status(200).send(route);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  },
+  shortestDistance(line, point) {
+    let shortestDistance = 0;
+    let shortestPoint = [];
+    for (let i = 0; i < line.length; i++) {
+      const temp = distance(point, line[i]);
+      if(temp < shortestDistance){
+        shortestDistance = temp;
+        shortestPoint = line[i];
+      }
+    }
+    return shortestPoint;
   }
 };
